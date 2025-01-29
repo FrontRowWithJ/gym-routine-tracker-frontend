147a3475131d9cce1101d901e92b5a9a10834e2e
frontrowwithj <adebusum@tcd.ie>
2025-01-29 04:04:23 +0000

    fixed origin issue

--- src/misc/constants.ts
+++ src/misc/constants.ts
@@ -4,7 +4,7 @@ export const YOUTUBE_ID_LENGTH = 11;
 export const ORIGIN =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001"
-    : "machrino.me";
+    : "https://machrino.me";
 export const OFFLINE_USER_ID = -1;
 export const FIVE_MINUTES = 300;
 export const DEFAULT_ERROR_MESSAGE = "Server error. Try again later.";
